191a1f3804d7afd66a3d63795e45ac558083cab5
somiceast <1474274420@qq.com>
2018-12-09 03:24:10 +0800

    beta 2.2 recorrected some faults,

--- IDLastSixNumber/python/createDic0.py
+++ IDLastSixNumber/python/createDic0.py
@@ -1,12 +1,13 @@
 def number_last_six_number():
     file_write_obj = open(
+        # "../output_data/female_idcard_lastsixnumber_data.txt", "w+") #female
         "../output_data/male_idcard_lastsixnumber_data.txt", "w+")
     print("open")
-    for day in range(1, 31, 1):
-        for lastthree_number in range(1, 999, 2):
-            # for lastthree_number in range(1, 999, 2): female id card last six
+    for day in range(1, 32, 1):
+        # for lastthree_number in range(0, 999, 2): #female
+        for lastthree_number in range(1, 1000, 2):
             # number
-            for last_number in range(0, 9, 1):
+            for last_number in range(0, 10, 1):
                 number = ("%02d" % day) + ("%03d" %
                                            lastthree_number) + ("%01d" % last_number) + "\n"
                 file_write_obj.write(number)
